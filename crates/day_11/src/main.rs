3e7043c96d91ad37bd78b50a1cdce753770918f3
Judah Perez <me@judahperez.com>
2023-12-11 22:11:11 -0800

    day 11 initial attempt with a star pathfinding

---
+++ crates/day_11/src/main.rs
@@ -0,0 +1,232 @@
+use std::{fs, path::PathBuf, collections::{HashSet, HashMap}};
+use pathfinding::prelude::astar;
+
+struct UnexpandedSpace {
+    space: Vec<char>,
+    width: usize,
+    height: usize,
+    rows_to_expand: HashSet<usize>,
+    columns_to_expand: HashSet<usize>,
+}
+
+struct ExpandedSpace {
+    galaxies: HashMap<Vector2, Galaxy>,
+}
+impl ExpandedSpace {
+    /// What moves are valid fom this position
+    fn get_successors(&self, p: &Vector2) -> Vec<(Vector2, u32)> {
+
+        //cardinal values
+        let mut successors = vec![];
+        if p.x > 0 {
+            successors.push(Vector2 {x: p.x - 1, y: p.y});
+        }
+        if p.y > 0 {
+            successors.push(Vector2 {x: p.x, y: p.y - 1});
+        }
+
+        successors.push(Vector2 {x: p.x + 1, y: p.y});
+        successors.push(Vector2 {x: p.x, y: p.y + 1});
+
+        successors.into_iter().map(|p| {
+            //let mut bias = 1;
+            //if self.galaxies.contains_key(&p) {
+            //    bias = 100;
+            //}
+            (p, 1)
+        }).collect()
+    }
+}
+
+struct Galaxy {
+    position: Vector2,
+    id: u32,
+}
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Vector2 {
+    x: u32,
+    y: u32,
+}
+
+impl Vector2 {
+    fn distance(&self, other: &Vector2) -> u32 {
+        (self.x.abs_diff(other.x) + self.y.abs_diff(other.y)) as u32
+      }
+    
+}
+
+fn main() {
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("assets\\input.txt");
+
+    let file_path = d.display().to_string();
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let val = process(contents);
+    println!("Sum Total {}", val);
+}
+
+fn process(contents: String) -> u32 {
+
+    // Do an expansion pass and then look up pass.
+    
+    let unexpanded_space = parse_contents(&contents);
+
+    let expanded_space = expand_space(&unexpanded_space);
+
+    let shortest_paths = get_shortest_paths(&expanded_space);
+
+    let mut total = 0;
+
+    for path in shortest_paths {
+        total += path;
+    }
+
+    total
+}
+
+fn parse_contents(contents: &String) -> UnexpandedSpace {
+
+    let mut space_lines = Vec::with_capacity(contents.len());
+    let mut columns_expand = HashSet::new();
+    let mut rows_expand = HashSet::new();
+    let lines = contents.lines();
+    let mut add_columns = true;
+    let mut width = 0;
+    let mut height = 0;
+    for (row, line) in lines.enumerate() {
+        let mut row_empty_space = true;
+        height += 1; // Naive height, just set each time we iterate
+        for (column, c) in line.chars().enumerate() {
+            if column >= width {
+                width = column + 1;
+            }
+
+            space_lines.push(c);
+            if c != '.' {
+                row_empty_space = false;
+                columns_expand.remove(&column);
+            }
+            else if add_columns {
+                //empty column, add for the first time.
+                columns_expand.insert(column);
+            }
+        }
+        if row_empty_space {
+            rows_expand.insert(row);
+        }
+        //only add columns for the first row
+        add_columns = false;
+    }
+
+    UnexpandedSpace {
+        space: space_lines,
+        width: width,
+        height: height,
+        rows_to_expand: rows_expand,
+        columns_to_expand: columns_expand,
+    }
+}
+
+fn expand_space(unexpanded_space: &UnexpandedSpace) -> ExpandedSpace {
+    
+    let mut galaxies =  HashMap::new();
+    let mut expanded_x: u32 = 0;
+    let mut expanded_y: u32 = 0;
+    let mut galaxy_id = 1;
+
+    for y in 0..unexpanded_space.height {
+        for x in 0..unexpanded_space.width {
+            let space_tile = unexpanded_space.space[y * unexpanded_space.height + x];
+
+            if space_tile == '#' {
+                let pos = Vector2 { x: expanded_x, y: expanded_y};
+                galaxies.insert(pos.clone(), Galaxy {
+                    id: galaxy_id,
+                    position: pos,
+                });
+                galaxy_id += 1;
+            }
+
+            if unexpanded_space.rows_to_expand.contains(&x) {
+                expanded_x += 1;
+            }
+
+            expanded_x += 1;
+        }
+
+        if unexpanded_space.columns_to_expand.contains(&y) {
+            expanded_y += 1;
+        }
+
+        expanded_y += 1;
+    }
+
+    ExpandedSpace { galaxies }
+}
+
+fn get_shortest_paths(expanded_space: &ExpandedSpace) -> Vec<u32> {
+
+    let mut shortest_paths = vec![];
+
+    let galaxies: Vec<&Galaxy> = expanded_space.galaxies.values().collect();
+    for g1 in 0..galaxies.len() {
+        let galaxy1 = galaxies[g1];
+
+        // Go through each galaxy pair until none are left to be paired
+        for g2 in g1+1..galaxies.len() {
+            let galaxy2 = galaxies[g2];
+
+            let result = astar(
+                &galaxy1.position, 
+                |p| expanded_space.get_successors(&p),
+                |p| p.distance(&galaxy2.position) / 2,
+                |p| *p == galaxy2.position);
+                let nodes: u32 = result.expect("no path found.").0.len().try_into().unwrap();
+                //remove the begining and end node count.
+                shortest_paths.push(nodes - 2)
+        }
+    }
+
+    shortest_paths
+}
+
+#[cfg(test)]
+mod day_11_tests {
+    use std::{fs, path::PathBuf};
+
+    use crate::process;
+
+    #[test]
+    fn input_test() {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("assets\\input.txt");
+
+        let file_path = d.display().to_string();
+
+        let _contents =
+            fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+        //let val = process(contents.clone());
+        //assert_ne!(0, val, "Value should not be 0.");
+        //assert!(13 < val, "Value is larger.");
+
+    }
+
+    #[test]
+    fn example1_test() {
+        let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        d.push("assets\\example.txt");
+
+        let file_path = d.display().to_string();
+
+        let contents =
+            fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+        let val = process(contents.clone());
+        assert_eq!(374, val);
+    }
+
+}
